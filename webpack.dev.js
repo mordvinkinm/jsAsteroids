d247a48a79b6e21d9d4d4cbac862e0c73db0ccfa
Mikhail Mordvinkin <mordvinkinm@gmail.com>
2019-02-10 16:41:48 -0600

    Added webpack-cli, updated webpack configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
+    mode: 'development',
     output: {
         filename: 'release/app.bundle.js'
     },
